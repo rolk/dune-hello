52de2ffd9b6969efa1efd652a3ca7149426cafc5
Roland Kaufmann <roland.kaufmann@uni.no>
2013-02-18 11:08:05 +0100

    Inline class documentation

--- dune/hello/hello.hpp
+++ dune/hello/hello.hpp
@@ -1,9 +1,22 @@
 #ifndef __DUNEHELLO_INCLUDED__
 #define __DUNEHELLO_INCLUDED__
+/**
+ * @file
+ *
+ * Social conversation protocol.
+ */
 
 namespace Dune {
   namespace Hello {
+	/**
+	 * Algebra for greetings.
+	 */
 	struct Hello {
+	  /**
+	   * Do the greeting operation.
+	   *
+	   * @return Meaning of life, universe and everything
+	   */
 	  int operator() ();
 	};
   }
